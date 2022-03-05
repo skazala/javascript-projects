ef7749dc30067ef2e0ace6e4ba476c605b39ff69
Yuliia Blahoveshchenska <87758198+skazala@users.noreply.github.com>
2022-03-05 18:51:29 +0300

    Create dog_years.js

---
+++ dog_years.js
@@ -0,0 +1,9 @@
+const myAge = 36;//human's age
+let earlyYears = 2;//this number will be multiplied by 10.5
+earlyYears *= 10.5;//first two human years converted into dog years
+let laterYears = myAge - 2;
+laterYears *= 4;//converting the rest human years into dog years
+console.log(`Early years are equal to ${earlyYears} and later years are equal to ${laterYears}.`);
+let myAgeInDogYears = earlyYears + laterYears;
+myName = 'Yuliia'.toLowerCase();//stransforms string to lowercase
+console.log(`My name is ${myName}, I am ${myAge} years old in human age which is ${myAgeInDogYears} years old in dog years.`);
